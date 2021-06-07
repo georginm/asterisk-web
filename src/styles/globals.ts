36b5967824a75f1b4d7d3ea7625b48690c394507
georginmatheus <georginmatheus@gmail.com>
2021-06-07 15:33:40 -0300

    Adição de estilos globais

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,6 +1,4 @@
 import { createGlobalStyle } from 'styled-components';
-// import asteriskLogo from '../assets/asterisk-logo.png';
-// background: #F0F0F5 url(${asteriskLogo}) no-repeat 70% top
 
 export default createGlobalStyle`
     * {
@@ -15,22 +13,66 @@ export default createGlobalStyle`
         -webkit-font-smoothing: antialiased;
     }
 
-    body, input, button {
+    body, input, button, a {
         font: 16px Roboto, sans-serif;
     }
 
     h1 {
         font-size: 48px;
-        color: #1e2d69;
+        color: #9C9C9C;
         margin-top: 80px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        line-height: 56px;
     }
+
     #root {
-        max-width: 700px;
+        max-width: 900px;
         margin: 0 auto;
         padding: 40px 20px;
     }
 
-    button {
+    button, a {
         cursor: pointer;
+    }
+
+    form {
+        margin-top: 40px;
+        max-width: 100%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    input {
+        margin-top: 20px;
+        width: 40%;
+        color: #3a3a3a;
+        height: 40px;
+        border: 0;
+        border-radius: 5px;
+        padding-left: 10px;
+        &::placeholder {
+            color: #a8a8b3;
+        }
+    }
+    button {
+        margin-top: 20px;
+        border-radius: 5px;
+        padding: 2px 5px;
+        width: 40%;
+        height: 40px;
+        background: #ff9e30;
+        color: #fdfefd;
+        border: 0;
+        font-weight: bold;
+        transition: background-color 0.2s;
+
+        &:hover {
+            background: #faae30;
+        }
     }
 `;
